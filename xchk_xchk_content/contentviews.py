a8fce115ea24692caba02ec4449326ca7cd91ee3
Vincent Nys <vincentnys@gmail.com>
2020-08-19 14:35:39 +0200

    Ontbrekend arg.

--- xchk_xchk_content/contentviews.py
+++ xchk_xchk_content/contentviews.py
@@ -14,7 +14,7 @@ class DemoXchkView(ContentView):
                 ("Lectoren wisselen probleemloos materiaal uit.",False,"Er zijn wel diensten zoals KlasCement, maar weet je altijd wat de verwachte voorkennis is,...?"),
                 ("Studenten leren fouten vaak pas vermijden nadat ze zelf gemaakt hebben.",True,"Heeft het veel zin een lijst met \"frequently made mistakes\" zonder context te geven?"),
                 ("Lectoren hebben meestal wel tijd voor differentiatie.",False,"Heb je rondgevraagd hoe veel collega's hier een prioriteit van kunnen maken?"))] 
-    _mc_answer_check = MultipleChoiceAnswerCheck(mc_data)
+    _mc_answer_check = MultipleChoiceAnswerCheck(filename=None,mc_data)
     custom_data = {'rendered_mc_qs': _mc_answer_check.render()}
     conditions = [FileExistsCheck(),MultipleChoiceFormatCheck(),_mc_answer_check]
     strat = Strategy(refusing_check=Negation(ConjunctiveCheck(conditions)),
