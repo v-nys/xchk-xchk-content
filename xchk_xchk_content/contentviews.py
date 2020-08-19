7359c44832f199d83581356b6939981ed5a990bf
Vincent Nys <vincentnys@gmail.com>
2020-08-19 14:15:39 +0200

    Eerste knoop.

--- xchk_xchk_content/contentviews.py
+++ xchk_xchk_content/contentviews.py
@@ -1,9 +1,19 @@
 from xchk_core.contentviews import ContentView
 from xchk_core.strats import *
+from xchk_multiple_choice_strategies.strats import MultipleChoiceAnswerCheck, MultipleChoiceFormatCheck
 
 class DemoXchkView(ContentView):
      
-    uid = 'xchk_xchk_content_demo'
-    template = 'xchk_xchk_content/xchk_xchk_content_demo.html'
-    strat = Strategy(refusing_check=TrueCheck(),
-                     accepting_check=Negation(TrueCheck()))
+    uid = 'xchk_xchk_content_motivation'
+    template = 'xchk_xchk_content/motivation.html'
+    mc_data = [("Welke uitspraak of uitspraken zijn waar?",
+                ("Studenten weten altijd precies wat verwacht wordt.",False,"Nog nooit foute bestanden gekregen? Zelfs geen foute bestandsnamen? Over het hoofd geziene leerstof?"),
+                ("Onmiddellijke feedback is effectiever dan uitgestelde feedback.",True,"Kan je twee weken na datum het gedachtenproces dat tot een fout heeft geleid nog vlot herhalen?"),
+                ("Lectoren wisselen probleemloos materiaal uit.",False,"Er zijn wel diensten zoals KlasCement, maar weet je altijd wat de verwachte voorkennis is,...?"),
+                ("Studenten leren fouten vaak pas vermijden nadat ze zelf gemaakt hebben.",True,"Heeft het veel zin een lijst met \"frequently made mistakes\" zonder context te geven?"),
+                ("Lectoren hebben meestal wel tijd voor differentiatie.",False,"Heb je rondgevraagd hoe veel collega's hier een prioriteit van kunnen maken?"))] 
+    _mc_answer_check = MultipleChoiceAnswerCheck(mc_data)
+    custom_data = {'rendered_mc_qs': _mc_answer_check.render()}
+    conditions = [FileExistsCheck(),MultipleChoiceFormatCheck(),_mc_answer_check]
+    strat = Strategy(refusing_check=Negation(ConjunctiveCheck(conditions)),
+                     accepting_check=TrueCheck())
